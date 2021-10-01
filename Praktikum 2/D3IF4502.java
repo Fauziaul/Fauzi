e70adf1d93e072276c95d27fd2dba5afd96862b5
Fauzi Aulia <auliafauzi835@gmail.com>
2021-10-01 09:26:36 +0700

    D3IF4502.java

---
+++ Praktikum 2/D3IF4502.java
@@ -0,0 +1,8 @@
+public class D3IF4502 {
+
+	public static void main(String[] args) {
+		System.out.println("Nama  :Fauzi Aulia!");
+		System.out.println("Nim   :6706210097!");
+		System.out.println("Kelas :D3IF4502!");
+	}
+}
